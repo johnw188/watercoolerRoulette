3500d905f431a0c8f7a85e2cc949214337eb5706
John Welsh <john.welsh@gmail.com>
2020-03-20 23:17:17 -0700

    Some initial UI work

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,25 +1,45 @@
 <script>
-	export let name;
+	import SignIn from './SignIn.svelte';
+	import SelfVideo from './SelfVideo.svelte';
 </script>
 
+<div class="mainContainer">
+<div class="video">
+	<SelfVideo/>
+</div>
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Watercooler Roulette</h1>
+	<SignIn/>
 </main>
+</div>
 
 <style>
+	.mainContainer {
+		background-color: #333333;
+		position: relative;
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
+	}
+	.video {
+		position: absolute;
+		top: 0;
+		width: 100%;
+	}
+
 	main {
+		position: absolute;
+		top: 0;
+		width: 100%;
 		text-align: center;
-		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
+		color: #ffffff;
 		font-size: 4em;
-		font-weight: 100;
+		font-weight: 600;
 	}
 
 	@media (min-width: 640px) {
---
+++ frontend/src/SelfVideo.svelte
@@ -0,0 +1,40 @@
+<div>
+	<video bind:this={video} autoplay="true" id="videoElement">
+	</video>
+</div>
+
+<style>
+    div {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        -webkit-filter: blur(15px);
+        -moz-filter: blur(15px);
+        -o-filter: blur(15px);
+        -ms-filter: blur(15px);
+        filter: blur(15px);
+    }
+    video {
+        width: 100%;
+    }
+</style>
+
+<script>
+import { onMount } from 'svelte';
+
+let video;
+
+onMount(() => {
+console.log("Video: "  + video)
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err) {
+      console.log("Something went wrong!" + err);
+    });
+}
+}
+)
+</script>

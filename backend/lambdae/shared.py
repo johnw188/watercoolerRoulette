50234a2318fb723b9e9e1f0da4d18ac5d9e0c2f0
meawoppl <meawoppl@gmail.com>
2020-03-21 04:39:39 -0700

    Typos fixed up

--- backend/lambdae/shared.py
+++ backend/lambdae/shared.py
@@ -4,6 +4,7 @@ import jwt
 
 
 def get_token_for_user():
+    # JWT whatnot in here...
     exp_dt = datetime.datetime.utcnow() + datetime.timedelta(days=1)
-    exipry = exp_dt.strftime("%a, %d %b %Y %H:%M:%S GMT")
-    return {"Set-Cookie": "foo=bar; Domain=watercooler.express; expires"}
+    expiry = exp_dt.strftime("%a, %d %b %Y %H:%M:%S GMT")
+    return {"Set-Cookie": "foo=bar; Domain=watercooler.express; expires=" + expiry}

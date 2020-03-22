4ad7ae69aacac01cccfc7da03800f8b1d4f1304a
meawoppl <meawoppl@gmail.com>
2020-03-22 13:15:50 -0700

    WIP

--- backend/lambdae/auth.py
+++ backend/lambdae/auth.py
@@ -1,16 +1,14 @@
+import functools
 import urllib.request
 import urllib.parse
 import os
 import traceback
 
 import lambdae.shared as shared
+import lambdae.models as models
 
 import requests
 import boto3
-
-USERS_TABLE = os.environ["USERS_TABLE"]
-CLIENT_ID = os.environ["OAUTH_ID"]
-CLIENT_SECRET = os.environ["OAUTH_SECRET"]
 
 EXPECTED_REDIRECT = "https://watercooler.express/chat"
 
@@ -19,42 +17,8 @@ AUTH_TEST_API = "https://api.slack.com/api/auth.test"
 USER_INFO_API = "https://slack.com/api/users.profile.get"
 
 
-# TODO: make me a helper generally
-def get_dynamo_table(table_name: str):
-    dynamodb = boto3.resource('dynamodb')
-    return dynamodb.Table(table_name)
-
-
-def get_or_create_user(
-        *,
-        user_id: str,
-        team_id: str,
-        slack_username: str,
-        slack_team: str,
-        slack_url: str,
-        slack_avatar: str):
-    # Hard overwrite, YOLO
-    table = get_dynamo_table(USERS_TABLE)
-    table.put_item(Item={
-        "user_id": user_id,
-        "team_id": team_id,
-        "slack_username": slack_username,
-        "slack_team": slack_team,
-        "slack_url": slack_url,
-        "slack_avatar": slack_avatar
-    })
-
-
+@shared.debug_wrapper
 def endpoint(event, context):
-    try:
-        return _endpoint(event, context)
-    except Exception as e:
-        return {
-            "statusCode": 200,
-            "body": str(e) + "\n" + traceback.format_exc()}
-
-
-def _endpoint(event, context):
     # This is the redirect behavior when slack fails to auth
     query_params = event["queryStringParameters"]
     if "error" in query_params:
@@ -64,8 +28,8 @@ def _endpoint(event, context):
 
     # Ask slack if user is legit
     auth_params = {
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET,
+        "client_id": os.environ["OAUTH_ID"],
+        "client_secret": os.environ["OAUTH_SECRET"],
         "code": query_params["code"],
         "redirect_uri": "https://watercooler.express/auth"
     }
@@ -86,7 +50,7 @@ def _endpoint(event, context):
     try:
         team_result = requests.post(AUTH_TEST_API, headers=headers).json()
     except urllib.error.URLError:
-        return {"statusCode": 403, "body": "Unable to get identity"}
+        return {"statusCode": 403, "body": "Unable to get identity!"}
 
     assert team_result["ok"], (token, team_result)
 
@@ -94,7 +58,7 @@ def _endpoint(event, context):
     try:
         profile_resp = requests.get(USER_INFO_API, headers=headers).json()
     except urllib.error.URLError:
-        return {"statusCode": 403, "body": "Unable to get avatar"}
+        return {"statusCode": 403, "body": "Unable to get avatar!"}
 
     assert profile_resp["ok"], profile_resp
 
@@ -108,7 +72,7 @@ def _endpoint(event, context):
     #     "user_id": "W12345678"
     # }
 
-    user = shared.UsersModel(
+    user = models.UsersModel(
         user_id=team_result["user_id"],
         team_id=team_result["team_id"],
         slack_username=team_result["user"],
@@ -118,7 +82,12 @@ def _endpoint(event, context):
     )
     user.save()
 
-    # Shoot the user a cookie with their JWT token, and redirect
-    response_headers = {"Location": "https://watercooler.express/chat"}
+    # Shoot the user a cookie with their JWT token, and redirect    
+    response_headers = {"Location": "https://watercooler.express/win"}
     response_headers.update(user.get_jwt_token_header())
     return {"statusCode": 302, "headers": response_headers}
+
+
+@shared.debug_wrapper
+def win(event, context):
+    return {"statusCode": 200, "body": "You win at oauth."}

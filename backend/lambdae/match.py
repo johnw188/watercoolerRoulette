f8e21a9fe4f64f64eee630c7bbd8cf1b76826cbf
meawoppl <meawoppl@gmail.com>
2020-03-23 03:23:04 -0700

    Working matacher endpoint

--- backend/lambdae/match.py
+++ backend/lambdae/match.py
@@ -7,18 +7,21 @@ import lambdae.shared as shared
 
 import pynamodb.exceptions
 
-INTERVAL_MS = 500
+INTERVAL_MS = 50
 MAX_TIME_S = 5.0
-TIMEOUT_REC_MS = 500
+TIMEOUT_REC_MS = 100
 
 
-def match_id_to_response(match: models.MatchesModel) -> dict:
-    return shared.json_success_response({"room_blob": match.room_blob}, 200)
+def get_timeout_rec_ms():
+    return TIMEOUT_REC_MS + int(random.uniform(0, 500))
+
+
+def match_id_to_response(partner: str, blob: dict) -> dict:
+    return shared.json_success_response({"partner": partner, "blob": blob})
 
 
 @shared.debug_wrapper
 def endpoint(event, context):
-
     start_time = time.time()
 
     event_dict = json.loads(event["body"])
@@ -38,35 +41,42 @@ def endpoint(event, context):
             # Try to claim the match, but only if unclaimed
             p_match.update(actions=actions, condition=HAS_NO_MATCH)
             print("Claimed a lover")
-            return match_id_to_response(models.MatchesModel.get(user.group_id, p_match.user_id))
+            
+            return match_id_to_response(p_match.user_id, p_match.room_blob)
         except pynamodb.exceptions.UpdateError:
             # User is already matched/deleted from the table
             continue
 
     # No luck matching to someone else, so put my record in the table, and wait on it
     waiting_match = models.MatchesModel(user.group_id, user.user_id)
+    waiting_match.match_id = None
+    waiting_match.room_blob = None
+
     waiting_match.save()
-
-    print("No match applied. Waiting on someone else")
-    while time.time() - start_time < MAX_TIME_S:
-        waiting_match.refresh()
-        if waiting_match.match_id is not None:
-            print("Was claimed!")
-            return match_id_to_response(waiting_match)
-        time.sleep(INTERVAL_MS / 1000)
-
-    # Try to delete the match record, but only if unmatched
     try:
-        waiting_match.delete(condition=models.MatchesModel.match_id.does_not_exist())
-    except pynamodb.exceptions.DeleteError:
-        print("Found love at last call")
-        return match_id_to_response(waiting_match)
+        print("No match applied. Waiting on someone else")
+        while time.time() - start_time < MAX_TIME_S:
+            waiting_match.refresh(consistent_read=True)
+            if waiting_match.match_id is not None:
+                print("Was claimed!")
+                return match_id_to_response(waiting_match.match_id, waiting_match.room_blob)
+            time.sleep(INTERVAL_MS / 1000)
+
+        # Try to delete the match record, but only if unmatched
+        try:
+            waiting_match.delete(condition=models.MatchesModel.match_id.does_not_exist())
+        except pynamodb.exceptions.DeleteError:
+            # NB(meawoppl) this case is likely untested
+            print("Found love at last call")
+            return match_id_to_response(waiting_match.match_id, waiting_match.room_blob)
+    finally:
+        waiting_match.delete()
 
     print("Exiting -> Timeout")
     return shared.json_error_response(
         message="Timed out waiting for match, try again in %ims!" % TIMEOUT_REC_MS,
         code=408,
-        j={"timeout_ms": TIMEOUT_REC_MS}
+        j={"timeout_ms": get_timeout_rec_ms()}
     )
 
 
@@ -76,4 +86,6 @@ def cleanup(event, context):
     old_condition = models.MatchesModel.created_dt > datetime.datetime.utcnow() - datetime.timedelta(minutes=15)
 
     with models.MatchesModel.batch_write() as batch:
-        list(map(batch.delete, models.MatchesModel.scan(filter_condition=old_condition)))
+        times = list(map(batch.delete, models.MatchesModel.scan(filter_condition=old_condition)))
+
+    print(times)
--- backend/match_tester.py
+++ backend/match_tester.py
@@ -1,6 +1,10 @@
-import json
+import concurrent.futures
+import time
+import uuid
+
 import requests
-import simplejson
+import numpy as np
+
 
 import lambdae.shared as shared
 
@@ -15,16 +19,60 @@ headers = {
 def request_match(group_id: str, user_id: str):
     # Forge myself a JWT token
     token = shared.jwt_encode({"group_id": group_id, "user_id": user_id})
-    response = requests.post(
-        "http://watercooler.express/match",
-        json={"token": token, "blob": {}},
-        headers=headers)
 
-    print("RESPONSE:")
-    try:
-        print(response.json())
-    except simplejson.errors.JSONDecodeError:
-        print(response.text)
+    start_time = time.time()
+    for x in range(20):
+        response = requests.post(
+            "http://watercooler.express/match",
+            json={"token": token, "blob": {"unique": str(uuid.uuid4())}},
+            headers=headers)
+
+        try:
+            results = response.json()
+        except Exception:
+            print(response.text)
+            raise
+
+        if(results["ok"]):
+            return (user_id, dict(time=time.time() - start_time, partner=results["partner"], blob=results["blob"], cycles=x+1))
+        else:
+            secs = results["timeout_ms"] / 1000
+            print("Wating for %fs" % secs)
+            time.sleep(secs)
+
+    return None
 
 
-request_match("faketeam", "fake38")
+fake_teammates = ["fake%i" % i for i in range(100)]
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=50) as tpe:
+    results = dict([r for r in tpe.map(lambda teammate: request_match("faketeam", teammate), fake_teammates)])
+
+
+# Stupid check the results
+for my_id, v in results.items():
+    print(my_id, v)
+
+for my_id, v in results.items():
+    my_blob = v["blob"]
+    my_partner = v["partner"]
+
+    partners_blob = results[my_partner]["blob"]
+    partners_partner = results[my_partner]["partner"]
+
+    assert my_blob == partners_blob
+    assert my_id == partners_partner
+
+    print("ID: {} <---matches---> ID: {}. Blobs identical.".format(my_id, my_partner))
+
+# Print some precentiles on delivery times
+times = list(r["time"] for r in results.values())
+times.sort()
+
+print("Average of %f cycles" % np.mean(list(r["cycles"] for r in results.values())))
+
+
+print("Timing info")
+print(times)
+for percentile in [0, 25, 50, 75, 100]:
+    print("%ith percentile finished in %ims" % (percentile, np.percentile(times, percentile)*1000))

68a738dc80adbee6938b99f5179ec8394a4b7b99
meawoppl <meawoppl@gmail.com>
2020-03-26 14:59:14 -0700

    Checking out in testing

--- backend/lambdae/jwt_tokens_test.py
+++ backend/lambdae/jwt_tokens_test.py
@@ -1,3 +1,5 @@
+from http import cookies
+
 import lambdae.models as models
 import lambdae.jwt_tokens as tokens
 
@@ -13,4 +15,11 @@ def test_jwt_encoding():
 def test_get_jwt_cookie():
     group, user = "fakegroup-cookie", "fakegroup-user"
     user = models.UsersModel(group, user)
-    tokens.get_jwt_cookie(user)
+    cookie = tokens.get_jwt_cookie(user)
+    assert cookie.startswith("token="), cookie
+
+    auth_cookie = cookies.SimpleCookie()
+    auth_cookie.load(cookie)
+    value = auth_cookie[tokens.COOKIE_ATTR_NAME].value
+    
+    tokens.jwt_decode(value)

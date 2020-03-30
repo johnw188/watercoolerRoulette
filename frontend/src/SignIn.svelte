cf4a1ac6c48ee10b81fc7fa7a8f8123672418a1f
John Welsh <john.welsh@gmail.com>
2020-03-30 13:04:03 -0700

    finished signin component

--- frontend/src/SignIn.svelte
+++ frontend/src/SignIn.svelte
@@ -1,6 +1,9 @@
 <script>
     import jwtDecode from 'jwt-decode';
     import cookies from 'js-cookie';
+   	import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
 
     let user = { loggedIn: false }
 
@@ -15,7 +18,7 @@
 
 <main>
     {#if user.loggedIn}
-    <button>Do the thing!</button>
+    <button on:click>Do the thing!</button>
     {:else}
     <a href="https://slack.com/oauth/authorize?scope=users.profile:read,users:read,users:read.email,identify&client_id=241546863697.1005448378753&redirect_uri=https%3A%2F%2Fapi.watercooler.express%2Fauth"><img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>
     {/if}

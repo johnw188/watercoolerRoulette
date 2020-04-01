95b2f6a145a528708ac5dc0c7d8187e9e7328393
meawoppl <meawoppl@gmail.com>
2020-03-31 18:54:09 -0700

    WIO

--- frontend/src/RtcHelpers.ts
+++ frontend/src/RtcHelpers.ts
@@ -19,118 +19,120 @@ export default class RtcHelpers {
     private _webcamWaiter: Promise<MediaStream>;
 
     constructor(identity: string) {
-        this._identity = identity;
+      this._identity = identity;
 
-        const configuration = {
-            iceServers: [{
-                urls: 'stun:stun.l.google.com:19302'
-            }]
+      const configuration = {
+        iceServers: [{
+          urls: 'stun:stun.l.google.com:19302',
+        }],
+      };
+
+      this._rtc = new RTCPeerConnection(configuration);
+
+      // This promise resolved after ICE candidate transmission is done
+      this._iceWaiter = new Promise((resolve, reject)=>{
+        const _localIce = new Array<RTCIceCandidate>();
+        this._rtc.onicecandidate = (e)=>{
+          if (e.candidate !== null) {
+            _localIce.push(e.candidate);
+          } else {
+            resolve(_localIce);
+          }
         };
+      });
 
-        this._rtc = new RTCPeerConnection(configuration);
-        
-        this._iceWaiter = new Promise((resolve, reject)=>{
-            let _localIce = new Array<RTCIceCandidate>();
-            this._rtc.onicecandidate = (e)=>{
-                if(e.candidate !== null)
-                    _localIce.push(e.candidate);
-                else {
-                    resolve(_localIce);
-                }
-            };    
-        });
+      // Text signaling data channel
+      // Setup data channel and configs for when channel is established
+      this._channelsSetup = new Promise((resolve, reject)=>{
+        const dcConfig = {};
+        this._dcSend = this._rtc.createDataChannel('test-wcr', dcConfig);
+        this._dcSend.onmessage = (event)=>this.log('Message SEND:', event);
+        // this._dcSend.onopen = (event)=>this.log("Open SEND:", event);
+        this._dcSend.onclose = (event) => this.log('Close SEND:', event);
 
-        // Text signaling data channel
-        // Setup data channel and configs for when channel is established
-        this._channelsSetup = new Promise((resolve, reject)=>{
-            const dcConfig = {};
-            this._dcSend = this._rtc.createDataChannel('test-wcr', dcConfig);
-            this._dcSend.onmessage = (event)=>this.log("Message SEND:", event);
-            // this._dcSend.onopen = (event)=>this.log("Open SEND:", event);
-            this._dcSend.onclose = (event) => this.log("Close SEND:", event);
-    
-            this._rtc.ondatachannel = (event) => {
-                this._dcRecv = event.channel;
-                this._dcRecv.onmessage = (event)=> this.log("Message RECV", event)
-                this._dcRecv.onopen = (event)=>{
-                    this.log(" Open RECV:" + event);
-                    resolve();
-                };
-                this._dcRecv.onclose = (event) => this.log("Close RECV:", event);
-            }
-        });
-
-        this._remoteStreamWaiter = new Promise((resolve, reject)=>{
-            this._rtc.ontrack = (event) => resolve(event.streams.slice(0, event.streams.length));
-        });
-
-        var mediaConstraints = {
-            audio: true,            // We want an audio track
-            video: {
-                aspectRatio: {
-                    ideal: 1.333333     // 3:2 aspect is preferred
-                }
-            }
+        this._rtc.ondatachannel = (event) => {
+          this._dcRecv = event.channel;
+          this._dcRecv.onmessage = (event)=> this.log('Message RECV', event);
+          this._dcRecv.onopen = (event)=>{
+            this.log(' Open RECV:' + event);
+            resolve();
+          };
+          this._dcRecv.onclose = (event) => this.log('Close RECV:', event);
         };
-    
-        // Putting this here gets the user prompt up as soon as the module is loaded
-        this._webcamWaiter =  navigator.mediaDevices.getUserMedia(mediaConstraints);
+      });
+
+      // This promise comes live after receiving a remote stream
+      this._remoteStreamWaiter = new Promise((resolve, reject)=>{
+        this._rtc.ontrack = (event) => resolve(event.streams.slice(0, event.streams.length));
+      });
+
+      const mediaConstraints = {
+        audio: true, // We want an audio track
+        video: {
+          aspectRatio: {
+            ideal: 1.333333, // 3:2 aspect is preferred
+          },
+        },
+      };
+
+      // Putting this here gets the user prompt up as soon as the module is loaded
+      this._webcamWaiter = navigator.mediaDevices.getUserMedia(mediaConstraints);
     }
 
     private log(...args: any[]) {
-        console.log("Identity:" + this._identity)
-        args.map(console.log);
+      console.log('Identity:' + this._identity);
+      args.map(console.log);
     }
 
-    public async getOfferIce(): Promise<OfferIce> {  
-        // Must complete before ice init
-        await this._beginRTCVideoStream();
+    public async getOfferIce(): Promise<OfferIce> {
+      // Must complete before ice init
+      await this._beginRTCVideoStream();
 
-        let offer = await this._rtc.createOffer()
-        await this._rtc.setLocalDescription(offer);
-        let ice = await this._iceWaiter;
-        return {offer, ice};
+      const offer = await this._rtc.createOffer();
+      await this._rtc.setLocalDescription(offer);
+      const ice = await this._iceWaiter;
+      return {offer, ice};
     }
 
     private async addAllIceCandidates(candidates: Array<RTCIceCandidate>): Promise<void> {
-        await Promise.all(candidates.map((ic)=>{
-            this._rtc.addIceCandidate(ic).catch(this.log);
-        }));
+      await Promise.all(candidates.map((ic)=>{
+        this._rtc.addIceCandidate(ic).catch(this.log);
+      }));
     }
 
     public async offerIceToAnswerIce(offerIce: OfferIce): Promise<AnswerIce> {
-        await this._rtc.setRemoteDescription(offerIce.offer);
-        await this.addAllIceCandidates(offerIce.ice);
-        await this._beginRTCVideoStream();
+      await this._rtc.setRemoteDescription(offerIce.offer);
+      await this.addAllIceCandidates(offerIce.ice);
+      await this._beginRTCVideoStream();
 
-        let answer = await this._rtc.createAnswer();
-        await this._rtc.setLocalDescription(answer);
-        let ice = await this._iceWaiter;
-        return {answer, ice};
+      const answer = await this._rtc.createAnswer();
+      await this._rtc.setLocalDescription(answer);
+      const ice = await this._iceWaiter;
+      return {answer, ice};
     }
 
     public async setAnswerIce(answerIce: AnswerIce): Promise<void> {
-        await this._rtc.setRemoteDescription(answerIce.answer);
-        await this.addAllIceCandidates(answerIce.ice);
+      await this._rtc.setRemoteDescription(answerIce.answer);
+      await this.addAllIceCandidates(answerIce.ice);
     }
 
     public async sendMessage(message: string): Promise<void> {
-        await this._channelsSetup;
-        this._dcSend.send(message);
+      await this._channelsSetup;
+      this._dcSend.send(message);
     }
 
     public async getRemoteVideoStream(): Promise<MediaStream> {
-        let streams = await this._remoteStreamWaiter;
-        return streams[0];
+      const streams = await this._remoteStreamWaiter;
+      return streams[0];
     }
 
     private async _beginRTCVideoStream(): Promise<void> {
-        let webcamStream = await this.getLocalVideoStream();
-        webcamStream.getTracks().forEach(
-            (track) => this._rtc.addTransceiver(track, {streams: [webcamStream]}));
+      const webcamStream = await this.getLocalVideoStream();
+      webcamStream.getTracks().forEach(
+          (track) => this._rtc.addTransceiver(track, {streams: [webcamStream]}));
     }
 
     public async getLocalVideoStream(): Promise<MediaStream> {
-        return this._webcamWaiter;
+      return this._webcamWaiter;
     }
 }
--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -3,96 +3,94 @@ export default class API {
     public static ANSWER_URL = 'https://api.watercooler.express/answer';
     public static USER_URL = 'https://api.watercooler.express/user';
 
-    constructor() {
-
-    }
+    constructor() { }
 
     private async _xhr_promise(method: string, url: string, data: object): Promise<XMLHttpRequest> {
-        return new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest();
+      return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
 
-            xhr.addEventListener("load", () => {
-                console.log('Loaded: ' + xhr.status);
-                console.log(xhr.response);
-                console.log();
-                if (xhr.status == 200) {
-                    // If successful
-                    resolve(xhr);
-                } else {
-                    reject(xhr)
-                }
-            });
-
-            xhr.open(method, url);
-            xhr.withCredentials = true;
-            xhr.responseType = "json"
-            xhr.send(JSON.stringify(data));
+        xhr.addEventListener('load', () => {
+          console.log('Loaded: ' + xhr.status);
+          console.log(xhr.response);
+          console.log();
+          if (xhr.status == 200) {
+            // If successful
+            resolve(xhr);
+          } else {
+            reject(xhr);
+          }
         });
+
+        xhr.open(method, url);
+        xhr.withCredentials = true;
+        xhr.responseType = 'json';
+        xhr.send(JSON.stringify(data));
+      });
     }
 
     private async _match(offer: object): Promise<string> {
-        return new Promise((resolve, reject)=>{
-            var xhr_promise: Promise<XMLHttpRequest> = this._xhr_promise("POST", API.MATCH_URL, {offer: offer});
+      return new Promise((resolve, reject)=>{
+        const xhr_promise: Promise<XMLHttpRequest> = this._xhr_promise('POST', API.MATCH_URL, {offer: offer});
 
-            xhr_promise.then((xhr)=>resolve(xhr.response.match_id));
-            xhr_promise.catch((xhr)=>{
-                if (xhr.status == 408) {
-                    console.log("Timeout during matching")
-                    reject({timeout_ms: xhr.response.timeout_ms})
-                } else {
-                    console.log("Unhandled fail :(")
-                    // If failed
-                    reject({
-                        status: xhr.status,
-                        statusText: xhr.statusText,
-                        response: xhr.response
-                    });
-                }
+        xhr_promise.then((xhr)=>resolve(xhr.response.match_id));
+        xhr_promise.catch((xhr)=>{
+          if (xhr.status == 408) {
+            console.log('Timeout during matching');
+            reject({timeout_ms: xhr.response.timeout_ms});
+          } else {
+            console.log('Unhandled fail :(');
+            // If failed
+            reject({
+              status: xhr.status,
+              statusText: xhr.statusText,
+              response: xhr.response,
             });
+          }
         });
+      });
     }
 
     private wait(timeout_ms: number): Promise<void> {
-        return new Promise((resolve, reject)=>{
-            setTimeout(()=>resolve(), timeout_ms)
-        })
+      return new Promise((resolve, reject)=>{
+        setTimeout(()=>resolve(), timeout_ms);
+      });
     }
 
     // Return a promise that completes on match
     public async match(offer: object): Promise<string> {
-        for(;;){
-            try{
-                return await this._match(offer);
-            } catch(e) {
-                if(e.timeout_ms){
-                    await this.wait(e.timeout_ms);
-                    console.log("Waiting " + e.timeout_ms + "ms");
-                } else {
-                    throw(e);
-                }
-            }
+      while(true) {
+        try {
+          return await this._match(offer);
+        } catch (e) {
+          if (e.timeout_ms) {
+            await this.wait(e.timeout_ms);
+            console.log('Waiting ' + e.timeout_ms + 'ms');
+          } else {
+            throw (e);
+          }
         }
+      }
     }
 
-    public async post_answer(answer: object): Promise<void>{
-        this._xhr_promise("POST", API.ANSWER_URL, {answer: answer});
+    public async post_answer(answer: object): Promise<void> {
+      this._xhr_promise('POST', API.ANSWER_URL, {answer: answer});
     }
 
     public async get_answer(): Promise<object> {
-        return new Promise((resolve, reject)=>{
-            this._xhr_promise("GET", API.ANSWER_URL, null).then(
-                    (xhr: XMLHttpRequest)=>resolve(xhr.response.answer)
-            )
-        });
+      return new Promise((resolve, reject)=>{
+        this._xhr_promise('GET', API.ANSWER_URL, null).then(
+            (xhr: XMLHttpRequest)=>resolve(xhr.response.answer),
+        );
+      });
     }
 
     public async get_user_info(user: string): Promise<object> {
-        return new Promise((resolve, reject)=>{
-            var url = API.USER_URL
-            url = user ? url + "/" + user: url;
-            this._xhr_promise("GET", url, null).then(
-                    (xhr: XMLHttpRequest)=>resolve(xhr.response)
-            );
-        });
+      return new Promise((resolve, reject)=>{
+        let url = API.USER_URL;
+        url = user ? url + '/' + user: url;
+        this._xhr_promise('GET', url, null).then(
+            (xhr: XMLHttpRequest)=>resolve(xhr.response),
+        );
+      });
     }
 }

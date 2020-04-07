ee4711c380c0c47ef7c983affcbba679c97eadb5
John Welsh <john.welsh@gmail.com>
2020-04-06 17:29:06 -0700

    Added spinner

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Spinner></Spinner>
     <div v-if="!loggedIn">
       <a href="https://slack.com/oauth/authorize?scope=users.profile:read,users:read,users:read.email,identify&client_id=241546863697.1005448378753&redirect_uri=https%3A%2F%2Fapi.watercooler.express%2Fauth"><img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>
     </div>
@@ -39,8 +40,10 @@ import API from './lib/Api';
 import ChatInteraction from './lib/ChatInteraction';
 import RtcPair from './lib/RtcPair';
 
+import Spinner from './components/Spinner.vue';
+
 @Component({
-  components: {},
+  components: { Spinner },
 })
 export default class App extends Vue {
   displayState = 'PREINIT';
